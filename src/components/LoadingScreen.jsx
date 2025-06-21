9b45f9fa736915469972cffc9332812b19c06f06
Keshab Dahal <102454190+Ksub2@users.noreply.github.com>
2025-06-21 12:23:48 +0545

    Update LoadingScreen.jsx

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<WELCOME TO MY WORLD />";
+  const fullText = "<HELLO! />";
 
   useEffect(() => {
     let index = 0;
